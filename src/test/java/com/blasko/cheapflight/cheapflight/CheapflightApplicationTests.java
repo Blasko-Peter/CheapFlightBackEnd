8f1ab5d30784d95d76576179ad0637d2273bc054
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-05-01 16:31:29 +0200

    init

---
+++ src/test/java/com/blasko/cheapflight/cheapflight/CheapflightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blasko.cheapflight.cheapflight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheapflightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
