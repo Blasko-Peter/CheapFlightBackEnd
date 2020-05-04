a7636e9d33400281da587e20fcdbb5a9a88fd417
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-05-04 21:31:07 +0200

    Add new Readme text and checked Selenium

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CheapFlightBackEnd
+
+Egy egyszerű szerver, ami csak adatokat szolgáltat a React-ban írodott front end számára.
+
+Első körben saját adatbázist generálok, melyből a front end képes lesz adatokat lehívni.
+
+De a terv az, hogy amikor a front end működik, akkor Selenium segítségével, valós járatokat keressen és valós adatokkal lássa el a React-os front endet.
+ 
--- src/main/java/com/blasko/cheapflight/cheapflight/controller/DataController.java
+++ src/main/java/com/blasko/cheapflight/cheapflight/controller/DataController.java
@@ -1,5 +1,9 @@
 package com.blasko.cheapflight.cheapflight.controller;
 
+import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +17,18 @@ public class DataController {
 
     String cities[] = new String[] {"Budapest", "Paris", "Madrid", "London", "Berlin"};
 
-    @GetMapping(value = "/getAllCities")
+    @GetMapping(value = "/getData")
     public List<String> getAllCities() {
         List<String> allCities = Arrays.asList(cities);
         return allCities;
+    }
+
+    @GetMapping(value = "/")
+    public String checkFirefoxBySelenium(){
+        FirefoxDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+        WebDriver x = new FirefoxDriver();
+        x.get("https://group.accor.com/en");
+        return "good";
     }
 
 }
