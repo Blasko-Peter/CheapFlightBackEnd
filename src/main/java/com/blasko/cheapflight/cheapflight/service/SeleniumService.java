2abd5782d6dae2c0f9a3753dfa9c898957d293ba
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-05-16 21:32:58 +0200

    cad add to startTown to website

--- src/main/java/com/blasko/cheapflight/cheapflight/service/SeleniumService.java
+++ src/main/java/com/blasko/cheapflight/cheapflight/service/SeleniumService.java
@@ -3,6 +3,8 @@ package com.blasko.cheapflight.cheapflight.service;
 import com.blasko.cheapflight.cheapflight.model.Flight;
 import com.blasko.cheapflight.cheapflight.model.FlightsRequest;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.stereotype.Service;
@@ -18,15 +20,26 @@ public class SeleniumService {
 
     public List<Flight> getActualFlights(FlightsRequest flightsRequest) {
         flights = new ArrayList<>();
-        checkedWebsiteBySelenium(flightsRequest.getStartTown(), flightsRequest.getArriveTown(), flightsRequest.getStartTime());
+        try {
+            checkedWebsiteBySelenium(flightsRequest.getStartTown(), flightsRequest.getArriveTown(), flightsRequest.getStartTime());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return flights;
     }
 
-    private void checkedWebsiteBySelenium(String startTown, String arriveTown, LocalDate date){
+    private void checkedWebsiteBySelenium(String startTown, String arriveTown, LocalDate date) throws InterruptedException {
         //Open the website
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.cheapoair.com/");
+        //Print startTown
+        driver.findElement(By.id("from0")).sendKeys(startTown);
+        Thread.sleep(1000);
+        driver.findElement(By.id("from0")).sendKeys(Keys.ENTER);
+
+
+
         //Close the website
         driver.close();
     }
